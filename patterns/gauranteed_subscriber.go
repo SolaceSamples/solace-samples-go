47df22c308770beb7f2a353dca4f98b86415409d
Tamimi <tamimi@solace.com>
2021-12-24 18:48:08 -0800

    add gauranteed subscriber

---
+++ patterns/gauranteed_subscriber.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
+
+	"solace.dev/go/messaging"
+	"solace.dev/go/messaging/pkg/solace/config"
+	"solace.dev/go/messaging/pkg/solace/message"
+	"solace.dev/go/messaging/pkg/solace/resource"
+)
+
+// Message Handler
+func MessageHandler(message message.InboundMessage) {
+	fmt.Printf("Message Dump %s \n", message)
+}
+
+func getEnv(key, def string) string {
+	if val, ok := os.LookupEnv(key); ok {
+		return val
+	}
+	return def
+}
+
+func main() {
+
+	// Configuration parameters
+	brokerConfig := config.ServicePropertyMap{
+		config.TransportLayerPropertyHost:                getEnv("SOLACE_HOST", "tcp://localhost:55554"),
+		config.ServicePropertyVPNName:                    getEnv("SOLACE_VPN", "default"),
+		config.AuthenticationPropertySchemeBasicPassword: getEnv("SOLACE_PASSWORD", "default"),
+		config.AuthenticationPropertySchemeBasicUserName: getEnv("SOLACE_USERNAME", "default"),
+	}
+
+	messagingService, err := messaging.NewMessagingServiceBuilder().FromConfigurationProvider(brokerConfig).Build()
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Connect to the messaging serice
+	if err := messagingService.Connect(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to the broker? ", messagingService.IsConnected())
+
+	if err != nil {
+		panic(err)
+	}
+
+	queueName := "sample-queue"
+	durable_exclusive_queue := resource.QueueDurableExclusive(queueName)
+
+	// Build a Gauranteed message receiver and bind to the given queue
+	persistentReceiver, err := messagingService.CreatePersistentMessageReceiverBuilder().WithMessageAutoAcknowledgement(true).Build(durable_exclusive_queue)
+
+	// Handling a panic from a non existing queue
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("Make sure queue name '%s' exists on the broker.\nThe following error occurred when attempting to connect to creat a Persistent Message Receiver:\n%s", queueName, err)
+		}
+	}()
+
+	// Start Direct Message Receiver
+	if err := persistentReceiver.Start(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Direct Receiver running? ", persistentReceiver.IsRunning())
+
+	// Register Message callback handler to the Message Receiver
+	if regErr := persistentReceiver.ReceiveAsync(MessageHandler); regErr != nil {
+		panic(regErr)
+	}
+	fmt.Printf("\n Bound to queue: %s\n", queueName)
+	fmt.Println("\n===Interrupt (CTR+C) to handle graceful terminaltion of the subscriber===\n")
+
+	// Run forever until an interrupt signal is received
+	// Handle interrupts
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	// Block until a signal is received.
+	<-c
+
+	// Terminate the Direct Receiver
+	persistentReceiver.Terminate(1 * time.Second)
+	fmt.Println("\nDirect Receiver Terminated? ", persistentReceiver.IsTerminated())
+	// Disconnect the Message Service
+	messagingService.Disconnect()
+	fmt.Println("Messaging Service Disconnected? ", !messagingService.IsConnected())
+
+}
